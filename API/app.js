559f730035fd2d64a8e60564977e9dd613185271
matty6223 <S3879003@student.rmit.edu.au>
2023-09-23 12:56:16 +1000

    Initialized friendships file structure

--- API/app.js
+++ API/app.js
@@ -16,18 +16,12 @@ app.use(function(req, res, next){
 // import routes
 const registerRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
+const friendshipsRoutes = require('./routes/friendships');
 
 //setup all the routes
 app.use('/register', registerRoutes);
 app.use('/Login', loginRoutes);
-
-
-
-app.get('/', function(req,res){
-    sql.connect(sqlConfig);
-    result = sql.query`select * from Accounts`
-    console.dir(result);
-})
+app.use('/friendships', friendshipsRoutes)
 
 //export the app
 module.exports = app;
