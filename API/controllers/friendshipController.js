7660cfba2322d3f70b490a02da4de2e9c090c63c
matty6223 <S3879003@student.rmit.edu.au>
2023-09-28 10:49:54 +1000

    refactored code to use controllers
    + Created the friendshipController.js class that implements the code functionality of the friendship system
    + Refactored friendship.js class to forward routes to the appropriate controller methods

---
+++ API/controllers/friendshipController.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const sql = require('mssql');
+const sqlConfig = require('../config');
+const { stringify } = require('querystring');
+
+//sends a friend request
+function sendRequest(requester, requestee, res){
+    //take in the requester and Requestee's Account ID
+    const friendship = {
+        Requester: requester,
+        requestee: requestee,
+        Status: "pending"
+    }
+
+    //submit the friendship to the database
+    try{
+        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
+            //check DB for an existing friendrequest
+            var result = await sql.query`Select * FROM Friendships
+                                           Where RequesterID = ${friendship.Requester}
+                                           AND AddresseeID = ${friendship.requestee}
+                                           
+                                           OR RequesterID = ${friendship.requestee} 
+                                           AND AddresseeID = ${friendship.Requester}`
+            
+            //if there isn't an existing friend request, add friendship into the database as pending
+            if(result.recordsets == 0){
+                result = await sql.query`INSERT INTO Friendships (RequesterID, AddresseeID, Status)
+                                         Values (${friendship.Requester}, ${friendship.requestee}, 'Pending')`
+                res.status(200).json({
+                    Message: "OK"
+                });
+            } else {
+                return res.status(400).json({
+                    Message: "Friendship already exists"
+                })
+            }
+        })
+    } catch{
+        return res.status(400).json({
+            Message: "Server Error"
+        })
+    }
+}
+
+function acceptRequest(currentUserID, OtherUserID, res){
+    try{
+        //Update the a friendship to accepted that has been sent by another user.
+        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
+            const result = await sql.query`UPDATE Friendships
+                                           SET Status = 'Accepted'
+                                           WHERE RequesterID = ${OtherUserID}
+                                           AND AddresseeID = ${currentUserID}`
+            return res.status(200).json({
+                Message: "OK"
+            });
+        });
+
+    }catch{
+        return res.status(400).json({
+            Message: "Server Error"
+        })
+    }
+}
+
+function deleteFriendship(currentUserID, otherUserID, res){
+    try{
+        //Delete a friendship or friendship request that has been sent by another user.
+        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
+            const result = await sql.query`DELETE FROM Friendships
+                                           WHERE RequesterID = ${otherUserID}
+                                           AND AddresseeID = ${currentUserID}
+                                           OR
+                                           AddresseeID = ${otherUserID}
+                                           AND RequesterID = ${currentUserID}`
+            return res.status(200).json({
+                Message: "OK"
+            });
+        });
+    }catch{
+        return res.status(400).json({
+            Message: "Server Error"
+        });
+    }
+}
+
+//return a list of users matching entered name
+function search(DisplayName, res){
+    try{
+        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
+            const result = await sql.query('SELECT * FROM Accounts WHERE DisplayName LIKE \'%' + DisplayName + '%\' ')
+
+            return res.status(200).json({
+                Message: "OK",
+                result
+            });
+        });
+    } catch{
+        return res.status(400).json({
+            Message: "Server Error"
+        })
+    }
+}
+
+//return a list of users friends or friendrequests
+function returnFriendsList(currentUserID, res){
+    try{
+        //select all users from the friendships table that match the users AccountID
+        //client side provides the requested status such as pending or accepted
+        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
+            const result = await sql.query`SELECT * FROM friendships
+
+                                           WHERE RequesterID = ${currentUserID}
+                                           OR AddresseeID = ${currentUserID}`
+            // console.dir();
+            const resultJson = JSON.parse(result.recordsets);
+
+            return res.status(200).json({
+                Message: "OK",
+                resultJson
+            });
+        });
+    }catch{
+        return res.status(400).json({
+            Message: "Server Error"
+        })
+    }
+}
+
+module.exports = {
+    sendRequest,
+    acceptRequest,
+    deleteFriendship,
+    search,
+    returnFriendsList
+}
+
--- API/routes/friendships.js
+++ API/routes/friendships.js
@@ -1,137 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const sql = require('mssql');
-const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const sqlConfig = require('../config');
 const { stringify } = require('querystring');
+const friendshipController = require('../controllers/friendshipController')
 
 var jsonparser = bodyParser.json();
 
 //send a friend request
 router.post('/', jsonparser, (req, res, next) => {
-    //take in the requester and Requestee's Account ID
-    const friendship = {
-        Requester: req.body.requester,
-        requestee: req.body.requestee,
-        Status: "pending"
-    }
+    //check if token is valid (TODO)
 
-    //submit the friendship to the database
-    try{
-        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
-            //check DB for an existing friendrequest
-            var result = await sql.query`Select * FROM Friendships
-                                           Where RequesterID = ${friendship.Requester}
-                                           AND AddresseeID = ${friendship.requestee}
-                                           
-                                           OR RequesterID = ${friendship.requestee} 
-                                           AND AddresseeID = ${friendship.Requester}`
-            
-            //if there isn't an existing friend request, add friendship into the database as pending
-            if(result.recordsets == 0){
-                result = await sql.query`INSERT INTO Friendships (RequesterID, AddresseeID, Status)
-                                         Values (${friendship.Requester}, ${friendship.requestee}, 'Pending')`
-                res.status(200).json({
-                    Message: "OK"
-                });
-            } else {
-                res.status(400).json({
-                    Message: "Friendship already exists"
-                })
-            }
-        })
-    } catch{
-        res.status(400).json({
-            Message: "Server Error"
-        })
-    }
+    //submit friend request to the controller which returns a response status.
+    friendshipController.sendRequest(req.body.requester, req.body.requestee, res);
+
 });
 
 //return users matching entered name
 router.get('/search', jsonparser, (req, res, next) => {
-    try{
-        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
-            const result = await sql.query('SELECT * FROM Accounts WHERE DisplayName LIKE \'%' + req.body.DisplayName + '%\' ')
+    //check if token is valid (TODO)
 
-            res.status(200).json({
-                Message: "OK",
-                result
-            });
-        });
-    } catch{
-        res.status(400).json({
-            Message: "Server Error"
-        })
-    }
+    //send search params to the controller, which returns a list in JSON
+    friendshipController.search(req.body.DisplayName, res);
 });
 
 //return a list of users friends or friendrequests
 router.get('/friends', jsonparser, (req, res, next) => {
-    try{
-        //select all users from the friendships table that match the users AccountID
-        //client side provides the requested status such as pending or accepted
-        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
-            const result = await sql.query`SELECT * FROM friendships
+    //check if token is valid (TODO)
 
-                                           WHERE RequesterID = ${req.body.currentUserID}
-                                           OR AddresseeID = ${req.body.currentUserID}`
-            // console.dir();
-            const resultJson = JSON.stringify(result.recordsets);
-
-            res.status(200).json({
-                Message: "OK",
-                resultJson
-            });
-        });
-    }catch{
-        res.status(400).json({
-            Message: "Server Error"
-        })
-    }
+    //send accountID to the controller, which returns a list of friendships in JSON
+    friendshipController.returnFriendsList(req.body.currentUserID, res);
 });
 
 //update the friendship request to accepted
 router.put('/accept', jsonparser, (req, res, next) => {
-    try{
-        //Update the a friendship to accepted that has been sent by another user.
-        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
-            const result = await sql.query`UPDATE Friendships
-                                           SET Status = 'Accepted'
-                                           WHERE RequesterID = ${req.body.OtherUserID}
-                                           AND AddresseeID = ${req.body.currentUserID}`
-            res.status(200).json({
-                Message: "OK"
-            });
-        });
+    //check if token is valid (TODO)
 
-    }catch{
-        res.status(400).json({
-            Message: "Server Error"
-        })
-    }
+    //send approval request to the controller, which returns a response
+    friendshipController.acceptRequest(req.body.currentUserID, req.body.OtherUserID, res);
 });
 
 //delete the friendship or friendship request
 router.delete('/delete', jsonparser, (req, res, next) => {
-    try{
-        //Delete a friendship or friendship request that has been sent by another user.
-        sql.connect(sqlConfig.returnServerConfig()).then(async function(){
-            const result = await sql.query`DELETE FROM Friendships
-                                           WHERE RequesterID = ${req.body.otherUserID}
-                                           AND AddresseeID = ${req.body.currentUserID}
-                                           OR
-                                           AddresseeID = ${req.body.otherUserID}
-                                           AND RequesterID = ${req.body.currentUserID}`
-            res.status(200).json({
-                Message: "OK"
-            });
-        });
-    }catch{
-        res.status(400).json({
-            Message: "Server Error"
-        });
-    }
+    //check if token is valid (TODO)
+    
+    //sends the delete request to the controller which returns a response
+    friendshipController.deleteFriendship(req.body.currentUserID, req.body.OtherUserID, res);
 });
 
 module.exports = router;
