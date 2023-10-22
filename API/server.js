04a1033b802d8e77faea51f7972abe28aa89efe5
Andrew Noorbergen <S3882566@student.rmit.edu.au>
2023-10-23 09:37:47 +1100

    Config SSL on back end server settings

--- API/server.js
+++ API/server.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const https_options = {
+    key: fs.readFileSync("C:/Users/Administrator/Desktop/certificate/private.key"),
+    cert: fs.readFileSync("C:/Users/Administrator/Desktop/certificate/certificate.crt"),
+    ca: fs.readFileSync("C:/Users/Administrator/Desktop/certificate/ca_bundle.crt")
+};
 const app = require('./app');
-const config = require('./config')
+const config = require('./config');
+const hostname = 'echo.matthewrosin.com';
 const port = 4000;
 
-const server = http.createServer(app);
+const server = https.createServer(https_options, app);
 const socketHandler = require('./middleware/socketHandler');
 
 socketHandler.initialiseSockets(server);
