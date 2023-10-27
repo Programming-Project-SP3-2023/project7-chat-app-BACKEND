a0a8a624b27ca98279ed76c8742fc8ef564ae144
matty6223 <S3879003@student.rmit.edu.au>
2023-10-27 14:28:21 +1000

    Created PeerServer to handle PeerJS functionality

--- API/server.js
+++ API/server.js
@@ -1,5 +1,5 @@
 //HTTPS
-// /*
+/*
 const https = require('https');
 const fs = require('fs');
 const https_options = {
@@ -13,21 +13,25 @@ const hostname = 'echo.matthewrosin.com';
 const port = 4000;
 
 const server = https.createServer(https_options, app);
-// */
+*/
 //HTTP
- /*
+
 const http = require('http');
 const fs = require('fs');
 const app = require('./app');
 const config = require('./config');
+const { ExpressPeerServer  } = require("peer");
+const socketHandler = require('./middleware/socketHandler');
 const port = 4000;
 
 const server = http.createServer(app);
- */
-const socketHandler = require('./middleware/socketHandler');
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: "/myapp",
+});
+
+app.use("/peerjs", peerServer);
 
 socketHandler.initialiseSockets(server);
-
-
 
 server.listen(port);
