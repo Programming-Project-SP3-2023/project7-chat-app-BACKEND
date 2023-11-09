e37451aac90357b28f4839cd1a735a4243ce3e6e
matty6223 <S3879003@student.rmit.edu.au>
2023-11-09 14:11:39 +1000

    Update adminController.js

--- API/controllers/adminController.js
+++ API/controllers/adminController.js
@@ -41,7 +41,7 @@ const updateAccount = async (req, res) => {
         sql.connect(sqlConfig.returnServerConfig()).then(async function(){
             //select results similar to the input display name entered
             const result = await sql.query
-            (`UPDATE Accounts SET Email = ${req.Email}, DisplayName = ${req.DisplayName}, Dob = ${req.Dob}
+            (`UPDATE Accounts SET Email = ${req.Email}, DisplayName = ${req.DisplayName}, Dob = ${req.Dob}, Accounts = ${req.Avatar}
             Where AccountID = ${req.AccountID}`);
             
             const userList = result.recordsets;
@@ -49,6 +49,10 @@ const updateAccount = async (req, res) => {
             if(result.rowsAffected > 0){
                 return res.status(200).json({
                     Message: "OK"
+                });
+            } else {
+                return res.status(400).json({
+                    Message: "Edit Failed"
                 });
             }
         });
@@ -95,6 +99,44 @@ const changePassword = async (req, res) => {
         });
     }
 }
+
+const adminLogin = async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    
+
+    try {
+        const pool = await sql.connect(sqlConfig.returnServerConfig());
+        // Get user's info by username
+        const result = await pool
+            .request()
+            .input('username', sql.NVarChar, username) // Corrected data type
+            .query('SELECT AccountID, PasswordHash FROM Logins WHERE Username = @username');
+        
+        if (result.recordset.length === 0) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        
+        //TODO: Check if isAdmin = 1;
+
+
+        const user = result.recordset[0];
+        // Compare provided password with stored hash
+        const isPasswordValid = await bcrypt.compare(password, user.PasswordHash);
+        
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        //Set web token with key taken from .env
+        const jwtSecret = process.env.JWT_SECRET;
+        const token = jwt.sign({ AccountID: user.AccountID, username: user.Username }, jwtSecret, { expiresIn: '1h' });
+        res.status(200).json({ message: 'Login Successful', token, AccountID: user.AccountID });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
 module.exports={
     getAccounts,
